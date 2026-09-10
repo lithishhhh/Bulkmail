67c9f112dd589beda974acb373e1a3fd6133cc70
lithishhhh <lithishv141@gmail.com>
2026-09-10 11:51:57 +0530

    Updated backend

--- backend/index.js
+++ backend/index.js
@@ -42,83 +42,82 @@ app.post("/api/login", (req, res) => {
 });
 
 
- mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log("Database connected");
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Database connected");
 
- }).catch((err)=>{
-    console.error("Error connecting to database:", err);
- });
+}).catch((err) => {
+  console.error("Error connecting to database:", err);
+});
 
- const credential = mongoose.model("credential",{},"bulkmail");
+const credential = mongoose.model("credential", {}, "bulkmail");
 
- 
-app.post("/sendemail", function(req, res) {
+
+app.post("/sendemail",async function (req, res) {
 
   var msg = req.body.msg;
   console.log(msg);
   var emailList = req.body.emailList;
   console.log(emailList);
 
-   var subject = req.body.subject;
+  var subject = req.body.subject;
   console.log(subject);
 
-  credential.find().then((data)=>{
+  credential.find().then((data) => {
 
     console.log("Credential data:", data)
-    
-
-  const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: data[0].toJSON().user,
-    pass: data[0].toJSON().pass,
-  },
-});
 
 
- new Promise( async (resolve,reject) =>{
-    try{
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: data[0].toJSON().user,
+        pass: data[0].toJSON().pass,
+      },
+    });
+
+
+    new Promise(async (resolve, reject) => {
+      try {
+
         for (var i = 0; i < emailList.length; i++) {
-    transporter.sendMail(
-    {
-      from: "lithishv141@gmail.com",
-      to: emailList[i],
-      subject: subject,
-      text: msg,
-    }
-   )
-     console.log("Email sent to " + emailList[i]);
-  
-  }
 
-   resolve("Success");
+          await transporter.sendMail({
+            from: "lithishv141@gmail.com",
+            to: emailList[i],
+            subject: subject,
+            text: msg,
+          });
 
-    }
-  
-    catch(error){
+          console.log("Email sent to " + emailList[i]);
+        }
+        resolve("Success");
 
-      reject("Failed");
+      }
 
-    }
-  })
-    .then(()=>{
-      res.send(true);
+      catch (error) {
+
+        reject("Failed");
+
+      }
     })
-    .catch(()=>{
-      res.send(false);
-    })
+      .then(() => {
+        res.send(true);
+      })
+      .catch(() => {
+        res.send(false);
+      })
   })
 
-}    
+}
 
 )
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, function(){
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
-  
+
 
